5b5d0243745c8f8fa0082dc7b4c55ab6762d0a91
kamioka24 <keee2404@gmail.com>
2020-08-24 21:30:51 +0900

    [first_commit]

---
+++ sample_test.rb
@@ -0,0 +1,14 @@
+# ファイル名はクラス名と合わせる。
+# ファイル名 → スネークケース
+# クラス名 → キャメルケース
+# クラス名はTestで始まるか終わる名前にすることが多い。(ファイル名と合わせる)
+
+require 'minitest/autorun' # ライブラリを読み込んでプログラム内でMinitestを使えるようにする
+
+class SampleTest < Minitest::Test # SampleTestクラスがMinitest::Testクラスを継承
+	def test_sample # Minitestはtest_で始まるメゾットを探して実行する為test_から始めることが必須となる（複数定義可能）
+		assert_equal 'RUBY', 'ruby'.upcase # 順番間違えないように
+	end
+end
+# assert_equalメゾットはMinitestが提供しているもの
+# assert_equal 期待する結果, テスト対象となる値や式
